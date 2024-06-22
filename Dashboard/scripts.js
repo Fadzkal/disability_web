90ef930d4390c1d2aeeec34ddd63ae69eee05259
SURAQIL <aqilabihar123@gmail.com>
2024-06-22 07:14:03 +0700

    benerin dikit mah ngga ngaruh

--- Dashboard/scripts.js
+++ Dashboard/scripts.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.menu-toggle');
     const sidebar = document.querySelector('.sidebar');
+    const dashboard = document.querySelector('.content');
 
     menuToggle.addEventListener('click', function() {
         sidebar.classList.toggle('open');
+    });
+    dashboard.addEventListener('click', function(event) {
+        if (sidebar.classList.contains('open') && !event.target.closest('.sidebar')) {
+            sidebar.classList.remove('open');
+        }
     });
 });
